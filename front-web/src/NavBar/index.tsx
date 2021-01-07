bb59c9ff74276db8594f811a881b86c47d2f896b
Nilton da Silva Junior <tonsti@gmail.com>
2021-01-07 20:51:12 +0100

    Add Navbar

---
+++ front-web/src/NavBar/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+import {ReactComponent as Logo} from './logo.svg';
+function NavBar() {
+    return (
+        <nav className="main-navbar">  
+        <Logo />
+        <a href="home" className="logo-text">DS Deliver</a>
+        </nav>
+    )
+}
+
+export default NavBar;
